77cbe5507112e7b08f9cffdb23a06e921e1d5592
Chandrashekar320 <anpchandrashekar@gmail.com>
2025-08-28 17:47:06 +0530

    commit prop and attribute binding

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Chandrashekhar',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Achyuth',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Martin Guptill',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'David Miller',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Kane Williamson',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Anoop Singh',
+    avatar: 'user-6.jpg',
+  },
+];
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-  userName:string='Chandrashekar';
+  userName:string='Chandrashekhar';
 }
