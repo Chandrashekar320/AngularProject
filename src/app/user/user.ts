354619aef5f13982d60e0e0e9043b1b68ee71f81
Chandrashekar320 <anpchandrashekar@gmail.com>
2025-08-29 13:49:36 +0530

    required true

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -11,8 +11,8 @@ import { Component, Input } from '@angular/core';
 export class User {
   
 
-@Input() avatar!: string;
-@Input() name!: string;
+@Input({required:true}) avatar!: string;
+@Input({required:true}) name!: string;
 get imagePath(){
 return '/users/' + this.avatar;
 }
