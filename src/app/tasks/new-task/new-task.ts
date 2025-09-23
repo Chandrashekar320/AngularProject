ebdcfd5f9334533f9d6f39ad735d8b06c28f7e47
Chandrashekar320 <anpchandrashekar@gmail.com>
2025-09-23 13:18:37 +0530

    54. Handling Form Submission.

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, signal } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,16 +10,16 @@ import { FormsModule } from '@angular/forms';
 export class NewTask {
   @Output() cancel = new EventEmitter<void>();
 
-//how we can use Two-way binding with signals
-  enteredTitle = signal('');
-  enteredSummary = signal('');
-  enteredDate = signal('');
-
-  // enteredTitle = '';
-  // enteredSummary = '';
-  // enteredDate = '';
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel(){
     this.cancel.emit();
   }
+
+  onSubmit() {
+    console.log("Form Submitted!");
+  }
+
 }
