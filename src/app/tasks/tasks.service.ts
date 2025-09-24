be7d2a0dac9ce797e55d8878895a5d5093e9ef3c
Chandrashekar320 <anpchandrashekar@gmail.com>
2025-09-24 15:16:28 +0530

    62. Using browser's localStorage for Storing tasks.

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { NewTaskData } from './task/task.model';
 import { NewTask } from './new-task/new-task';
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TasksService {
     private tasks = [
         {
@@ -36,22 +36,34 @@ export class TasksService {
         },
     ];
 
-    getUserTasks(userId: string) {
-       return this.tasks.filter((task) => task.userId === userId);
+    constructor() {
+        const tasks = localStorage.getItem('tasks');
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
     }
 
-    addTask(taskData: NewTaskData, userId: string)
-    {
+    getUserTasks(userId: string) {
+        return this.tasks.filter((task) => task.userId === userId);
+    }
+
+    addTask(taskData: NewTaskData, userId: string) {
         this.tasks.unshift({
             id: new Date().getTime().toString(),
             userId: userId,
             title: taskData.title,
             summary: taskData.summary,
             dueDate: taskData.date,
-        })
+        });
+        this.saveTasks();
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
+    }
+
+    saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
